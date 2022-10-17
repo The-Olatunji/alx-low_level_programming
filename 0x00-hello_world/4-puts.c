fc92ecf09339c3bc6ff805d4b5a135ac6f2397d4
The-Olatunji <olatunjitope02@gmail.com>
2022-10-17 00:57:31 +0000

    puts and printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+  *main - the function main
+  *Return: returns 0
+  */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
-	return(0);
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
 }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+  *main - main prints a string
+  *Return : returns 0, return string
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

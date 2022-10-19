42260360c9350cc25a0cb8e4de1ca7c1d407ae49
The-Olatunji <olatunjitope02@gmail.com>
2022-10-19 09:48:48 +0000

    create _putchar function

---
+++ 0x02-functions_nested_loops/putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  *_putchar - entry point
+  *@x: int x is the number to be checked
+  *Return: returns 0
+  */
+int _putchar (int x)
+{
+	putchar(x);
+	return (0);
+}

5b73f5a1e01b4a0a9b3a067cab3fdfdea7f52c9f
The-Olatunji <olatunjitope02@gmail.com>
2022-10-23 15:11:30 +0000

    Fizz-Buzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  *main - on multiples of 3 fizz, on multiples of 5 buzz...
+  *
+  *Return: 0
+  */
+
+int main(void)
+{
+	int i;
+	char f[] = "Fizz";
+	char b[] = "Buzz";
+	char fb[] = "FizzBuzz";
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i == 100)
+		{
+			printf("%s", b);
+		}
+		else if ((i % 3 == 0) && (i % 5 == 0))
+			printf("%s ", fb);
+		else if (i % 3 == 0)
+			printf("%s ", f);
+		else if (i % 5 == 0)
+			printf("%s ", b);
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}

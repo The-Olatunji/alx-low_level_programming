3975b402dafdf21953d6e6bfacaac373c2a8d2a0
The-Olatunji <olatunjitope02@gmail.com>
2022-10-26 01:47:21 +0000

    task-3 to task-6

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+  *rev_string - a function that reserves a string
+  *@s: input to reserve
+  *Return: string and n reverse
+  */
+void rev_string(char *s)
+{
+	char rev = s[0];
+	int fcounter = 0;
+	int i;
+
+	while (s[fcounter] != '\0')
+		fcounter++;
+
+	for (i = 0; i < fcounter; i++)
+	{
+		fcounter--;
+		rev = s[i];
+		s[i] = s[fcounter];
+		s[fcounter] = rev;
+	}
+}

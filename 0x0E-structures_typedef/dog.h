6347a75fd3bd12e40a432cb3f384ff3ac83b1f59
The-Olatunji <olatunjitope02@gmail.com>
2022-11-19 00:29:20 +0000

    a new struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+  *struct dog - a dog structure
+  *@name: first prop
+  *@float: second prop
+  *@owner: third
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif

5fbf911842f423c56d6bed4b91450b9465553d0b
The-Olatunji <olatunjitope02@gmail.com>
2022-11-20 14:45:43 +0000

    add new dog to struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,9 +16,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	char *temp_name;
 	char *temp_owner;
 
-	bingo = malloc(sizeof(dog_t));
 	temp_name = malloc(sizeof(*name) * (strlen(name) + 1));
 	temp_owner = malloc(sizeof(*owner) * (strlen(name) + 1));
+	bingo = malloc(sizeof(dog_t));
+
 	if (bingo && temp_name && temp_owner)
 	{
 		strcpy(temp_name, name);

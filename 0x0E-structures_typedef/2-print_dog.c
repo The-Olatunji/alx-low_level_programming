d44ce4e3cd3902f80b5fe4df9acb4f6e1cabf5f9
The-Olatunji <olatunjitope02@gmail.com>
2022-11-20 04:23:35 +0000

    print initialized element of struct dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  *print_dog - print out various variable of type Struct dog
+  *@d: a Struct
+  */
+void print_dog(struct dog *d)
+{
+
+	if (d == NULL)
+		printf("Nothing\n");
+
+	if (d->name != NULL)
+		printf("Name: %s\n", d->name);
+	else
+		printf("Name: (nil)\n");
+
+	printf("Age: %.6f\n", d->age);
+
+	if (d->owner != NULL)
+		printf("Owner: %s\n", d->owner);
+
+}
